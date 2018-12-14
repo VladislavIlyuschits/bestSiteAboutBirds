5c8c4ef3f21792405a01b887fa63b3fe27235a0d
Maksim Kazak <kazakmaksims@gmail.com>
2018-12-15 01:26:07 +0300

    Add modal to Belarus page

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-$( document ).ready(function() {
-    $('.').on('click', function(e) {
-        $('.side-nav').toggleClass("open");
-        e.preventDefault();
-    });
+let images = {
+  'Grodno': 'img/hrodno.jpeg',
+  'Vitebsk': 'img/vitebsk.jpg',
+  'Minsk': 'img/minsk.jpg',
+  'Mir': 'img/mir.jpg',
+  'Mohilev': 'img/mohilev.jpg',
+  'Brest': 'img/brest.jpg',
+};
+let texts = {
+  'Grodno': 'text for Grodno',
+  'Vitebsk': 'text for Vitebsk',
+  'Minsk': 'text for Minsk',
+  'Mir': 'text for Mir',
+  'Mohilev': 'text for Mohilev',
+  'Brest': 'text for Brest',
+};
+
+$('#exampleModal').on('show.bs.modal', function (event) {
+  let button = $(event.relatedTarget);
+  let city = button.data('city');
+  let modal = $(this);
+  modal.find('.modal-title').text(city);
+  let body = modal.find('.modal-body');
+  body.empty();
+  body.append(`<img style="max-width: 100%" src="${images[city]}"></img>`);
+  body.append(`<p>${texts[city]}</p>`);
 });
